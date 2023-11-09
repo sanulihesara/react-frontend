14fd7b1b415f27bc38767046461cba9f16c3e641
Nipuna Rambukkanage <nipunar@adelanka.com>
2023-11-09 21:12:44 +0530

    Updated the Site

--- src/components/MyAchievements.js
+++ src/components/MyAchievements.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 function MyAchievements(){
 
-    return <h2>This is my Achievements Page</h2>
+    return ( 
+        <div>
+        <h2>This is my Achievements Page</h2>
+    
+        <p>My first achievement is, passing the scholarship exam</p>
+        <p>My second achievement is, winning the school cricket trophy</p>
+        
+        <img src="https://www.nagroup.lk/wp-content/uploads/2018/05/1958154_229205460604399_1391449091_n.jpg" />
+        </div>
+        );
 
 }
 
