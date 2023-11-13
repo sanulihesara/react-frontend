a93b67e99c1e81bfb2e1c3919363ac85ae858059
Nipuna Rambukkanage <nipunar@adelanka.com>
2023-11-13 21:36:52 +0530

    Added additional Avatars

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -21,6 +21,36 @@ function UserProfile() {
 
                     </Grid>
                 </Grid>
+                <hr></hr>
+                <Grid container spacing={3} justifyContent="center" alignItems="center">
+                    <Grid item>
+                        <Avatar
+                            alt="My Username"
+                            src="https://lakeoktransport.ca/images/avataaars3.png"
+                            sx={{ width: 150, height: 150 }}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Typography color="textSecondary" variant='h3'> Sahan Sulakshana Silva </Typography>
+                        <Typography variant='h5'> Expertise : HTML, C#, Processing, React </Typography>
+                        <Typography > Hi my name is Sahan Sulakshana Silva. I am a Software Developer. </Typography>
+                    </Grid>
+                </Grid>
+                <hr></hr>
+                <Grid container spacing={3} justifyContent="center" alignItems="center">
+                    <Grid item>
+                        <Avatar
+                            alt="My Username"
+                            src="https://lakeoktransport.ca/images/avataaars4.png"
+                            sx={{ width: 150, height: 150 }}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Typography color="textSecondary" variant='h3'> Rusira Samarasekara </Typography>
+                        <Typography variant='h5'> Expertise : HTML, C#, Processing, React </Typography>
+                        <Typography > Hi my name is Rusira Samarasekara. I am a Software Developer. </Typography>
+                    </Grid>
+                </Grid>
             </CardContent>
         </Card>
     );
