5697f8b16d785e88725abe3c8d9f506e381ab4bc
Nipuna Rambukkanage <nipunar@adelanka.com>
2023-11-13 20:21:57 +0530

    Added changes to the MySchool page

--- src/components/MySchool.js
+++ src/components/MySchool.js
@@ -5,14 +5,6 @@ function MySchool(){
     return ( 
     <div>
     <h2>This is my School Page</h2>
-
-    <p>My School is Ananda College</p>
-    <p>My School is Ananda College</p>
-    <p>My School is Ananda College</p>
-    <p>My School is Ananda College</p>
-    <p>My School is Ananda College</p>
-
-    <img src="https://www.nagroup.lk/wp-content/uploads/2018/05/1958154_229205460604399_1391449091_n.jpg" />
     </div>
     );
 }
