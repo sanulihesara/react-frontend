4a980f2d833a284e9529aefd5506ca13fbd6c5d2
Nipuna Rambukkanage <nipunar@adelanka.com>
2023-11-20 21:00:06 +0530

    Added features to AboutMe Page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import styled from '@emotion/styled';
+import { Avatar, Card, CardContent, Grid, Slide, Typography } from '@mui/material';
+import React, { useState } from 'react';
+
+const AnimatedCard = styled(Card)`
+  transition : transform 0.3s ease-in-out;
+  &:hover{
+    transform : scale(1.05);
+  }
+  cursor : pointer;
+`;
 
 function AboutMe(){
+    const [expanded, setExpanded] = useState(false);
 
-    return <h2>This is my About Me Page</h2>
+    const handleCardClick = () => {
+        setExpanded(!expanded);
+    }
+
+    return (
+        <Grid container spacing={3} justifyContent="center">
+            <Grid item>
+                <Slide direction="right" in={true} timeout={5}>
+                    <AnimatedCard onClick={handleCardClick} 
+                    style={{transform: expanded ? 'scale(1.25)' : 'scale(1)' }}>
+                        <CardContent>
+                            <Avatar alt="My Avatar"
+                            src="https://lakeoktransport.ca/images/avataaars4.png"
+                            sx={{width: 80, height: 80, marginBottom : 2}} />
+                            <Typography variant="h6" gutterBottom>
+                                Hello My Friends! Welcome to my Web App!
+                            </Typography>
+                            <Typography>
+                                I am an enthusiastic Programmer. I hereby welcome you to my React Web Application. 
+                            </Typography>
+                        </CardContent>
+                    </AnimatedCard>
+                </Slide>
+            </Grid>
+
+        </Grid>
+    );
 
 }
 
